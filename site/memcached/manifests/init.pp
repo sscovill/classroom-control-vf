afe8edf0f7ba6a277d698574617cd23cae2277e5
sscovill <shawn.scoville@vertexinc.com>
2016-05-24 15:50:15 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,23 @@
+class memcached {
+
+package { 'memcached':
+ensure => present,
+before => File['/etc/sysconfig/memcached']
+}
+
+service { 'memcached':
+ensure => running,
+enable => true,
+subscribe => File['/etc/sysconfig/memcached.conf'],
+}
+
+file { '/etc/sysconfig/memcached':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/memcached/memcached',
+require => Package['memcached'],
+}
+
+}
