a5723340ac21da0c42e63fbb4ec415efdf6a027f
sscovill <shawn.scoville@vertexinc.com>
2016-05-24 16:52:06 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,19 +5,54 @@ ensure => present,
 before => File['/etc/nginx/nginx.conf'],
 }
 
-service { 'nginx':
-ensure => running,
-enable => true,
-subscribe => File['/etc/nginx/nginx.conf'],
+file { '/var/www':
+ensure => directory,
+owner => 'root',
+group => 'root',
+mode => '0775',
+}
+
+file { '/var/www/index.html':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0664',
+source => 'puppet:///modules/nginx/index.html',
 }
 
 file { '/etc/nginx/nginx.conf':
 ensure => file,
 owner => 'root',
 group => 'root',
-mode => '0644',
+mode => '0664',
 source => 'puppet:///modules/nginx/nginx.conf',
 require => Package['nginx'],
+notify => Service['nginx'],
+}
+
+file { '/etc/nginx/conf.d':
+ensure => directory,
+owner => 'root',
+group => 'root',
+mode => '0775',
+service { 'nginx':
+ensure => running,
+enable => true,
+subscribe => File['/etc/nginx/nginx.conf'],
+}
+file { '/etc/nginx/conf.d/default.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0664',
+source => 'puppet:///modules/nginx/default.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+
+service { 'nginx':
+ensure => running,
+enable => true,
 }
 
 }
