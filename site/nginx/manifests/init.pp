2355d02728afdf343b61fdf0457ea4fb2d948751
sscovill <shawn.scoville@vertexinc.com>
2016-05-24 16:33:46 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,23 @@
+class nginx {
 
+package { 'nginx':
+ensure => present,
+before => File['/etc/nginx/nginx.conf'],
+}
+
+service { 'nginx':
+ensure => running,
+enable => true,
+subscribe => File['/etc/nginx/nginx.conf'],
+}
+
+file { '/etc/nginx/nginx.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+}
+
+}
