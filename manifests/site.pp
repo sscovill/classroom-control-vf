e965781c68d58128a81368ddab7932bc0b7fb1b1
sscovill <shawn.scoville@vertexinc.com>
2016-05-25 10:26:53 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
+  }
 
 file {'motd':
   path    => '/etc/motd',
